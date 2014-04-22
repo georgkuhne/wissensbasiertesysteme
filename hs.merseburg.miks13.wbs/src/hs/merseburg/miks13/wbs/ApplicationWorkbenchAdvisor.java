825fdcb80da59b06e44012bc9f9a027402e39efc
9gkuhne <9gkuhne@149.205.196.117>
2014-04-22 15:32:50 +0200

    asdasd

---
+++ hs.merseburg.miks13.wbs/src/hs/merseburg/miks13/wbs/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package hs.merseburg.miks13.wbs;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "hs.merseburg.miks13.wbs.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
