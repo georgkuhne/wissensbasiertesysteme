7c62b15418874f8401ba5b18b8069258dbcb9e06
richard <richard@richard-Aspire-V5-573G>
2014-05-23 12:48:16 +0200

    regelgui

---
+++ hs.merseburg.miks13.wbs/src/hs/merseburg/miks13/wbs/gui/regel/DialogCreateNewRule.java
@@ -0,0 +1,63 @@
+package hs.merseburg.miks13.wbs.gui.regel;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+public class DialogCreateNewRule extends Dialog {
+
+	/**
+	 * Create the dialog.
+	 * 
+	 * @param parentShell
+	 */
+	public DialogCreateNewRule(Shell parentShell) {
+		super(parentShell);
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 * 
+	 * @param parent
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite container = (Composite) super.createDialogArea(parent);
+		container.setLayout(new FillLayout(SWT.HORIZONTAL));
+
+		Composite composite = new Composite(container, SWT.NONE);
+
+		Label lblName = new Label(composite, SWT.NONE);
+		lblName.setBounds(10, 10, 62, 15);
+		lblName.setText("Name:");
+
+		return container;
+	}
+
+	/**
+	 * Create contents of the button bar.
+	 * 
+	 * @param parent
+	 */
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL,
+				true);
+		createButton(parent, IDialogConstants.CANCEL_ID,
+				IDialogConstants.CANCEL_LABEL, false);
+	}
+
+	/**
+	 * Return the initial size of the dialog.
+	 */
+	@Override
+	protected Point getInitialSize() {
+		return new Point(647, 486);
+	}
+}
